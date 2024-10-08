5ffdf847bb072dfd486e9afeba4525c3d7fc7199
MVRT <alj.martinez.j@gmail.com>
2024-10-08 17:17:27 +0200

    auth settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"angular-test-01-9e843","appId":"1:368523944492:web:91fb9c9e3d90c7cb30a789","storageBucket":"angular-test-01-9e843.appspot.com","apiKey":"AIzaSyAe6jTdO0jxabHkfD5Ph1GGQyu1XeHpLd4","authDomain":"angular-test-01-9e843.firebaseapp.com","messagingSenderId":"368523944492","measurementId":"G-9R8QX0TFPZ"})), provideAuth(() => getAuth())]
 };
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +10,17 @@ import { Component, Input } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  [x: string]: any;
 
   @Input() title!: string;
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  signOut(){
+    this.authService.logout().then(() =>{
+      this.router.navigateByUrl('login');
+    })
+    this.authService.logout();
+  }
   
 }
